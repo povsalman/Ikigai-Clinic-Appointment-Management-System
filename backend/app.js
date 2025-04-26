4d87880013094017ed3eb1e91cf1ab6b3498ccd7
Salman Khan <salmankhanbhittani2@gmail.com>
2025-04-26 23:54:35 +0500

    Updated Doc Signup

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,8 @@ const app = express();
 const authRoutes = require('./routes/auth.routes');
 const patientRoutes = require('./routes/patient.routes');
 const adminRoutes = require('./routes/admin.routes');
-<<<<<<< HEAD
 const doctorRoutes = require('./routes/doctor.routes'); 
-=======
-const doctorRoutes = require('./routes/doctor.routes');
->>>>>>> 2c24abc8cda64c0c04a4e2f9fb24f94c6a982191
+
 const cors = require('cors');
 const morgan = require('morgan');
 
@@ -19,10 +16,6 @@ app.use(express.json());
 app.use('/api/admin', adminRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/patients', patientRoutes);
-<<<<<<< HEAD
-app.use('/api/doctors', doctorRoutes); 
-=======
 app.use('/api/doctors', doctorRoutes);
 
->>>>>>> 2c24abc8cda64c0c04a4e2f9fb24f94c6a982191
 module.exports = app;

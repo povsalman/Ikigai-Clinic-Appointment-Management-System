50cadb13754f80d09d8435bddf2c7f5010c617e9
Maryum Tanvir <maryumtanvir@gmail.com>
2025-04-27 13:32:51 +0500

    layout and dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import App from './App';
-import './index.css'; // Tailwind styles
-import 'antd/dist/reset.css'; // Latest versions use reset.css
-
-
-=======
 import App from './App.jsx';
 import './index.css';
 
->>>>>>> 49411cccacc26cf6f3250ff8d9c8da008c12beb0
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 49411cccacc26cf6f3250ff8d9c8da008c12beb0

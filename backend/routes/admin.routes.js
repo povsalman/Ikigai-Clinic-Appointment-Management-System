9e07b3e262d5dc70649037758b67a348edb7c0d0
Maryum Tanvir <maryumtanvir@gmail.com>
2025-04-27 21:18:14 +0500

    admin updates

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -4,10 +4,13 @@ const adminController = require('../controllers/admin.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
 
+router.use(authMiddleware)
 
-//GET admin profile
-router.get('/profile', authMiddleware, adminController.getAdminProfile);
+// GET admin profile
+router.get('/profile', adminController.getAdminProfile);
 
+// PUT update admin profile
+router.put('/profile', adminController.updateAdminProfile);
 
 // GET all pending doctor requests
 router.get('/doctor-requests', adminController.getPendingDoctorRequests);
@@ -21,8 +24,18 @@ router.put('/doctor-requests/:id/reject', adminController.rejectDoctor);
 // POST assign a shift to a doctor
 router.post('/assign-shift', adminController.assignShift);
 
+//GET shifts
+router.get('/shifts', adminController.getAllShifts);
+
 // GET all users (optional)
 router.get('/users', adminController.getAllUsers);
+
+// GET single doctor info
+router.get('/doctors/:id', adminController.getDoctorInfo);
+
+// GET all doctors
+router.get('/doctors', adminController.getAllDoctors);
+
 
 // DELETE a doctor profile
 router.delete('/doctors/:id', adminController.deleteDoctorProfile);

6dadcd030055115aa83170e8504a79aaac1e41c8
Mavra Mehak Gill <i220798@nu.edu.pk>
2025-04-28 17:40:17 +0500

    Updates

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -7,6 +7,7 @@ const appointmentSchema = new mongoose.Schema({
   time: { type: String, required: true },
   status: { type: String, enum: ["scheduled", "completed", "cancelled", "rescheduled"], required: true },
   notes: String,
+  hasFeedback: { type: Boolean, default: false },
   rescheduledFrom: { type: mongoose.Schema.Types.ObjectId, ref: 'Appointment' },
   createdAt: Date,
   updatedAt: Date
